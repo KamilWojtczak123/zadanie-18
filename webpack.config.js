a1f8aba20ff07d369971b1f39d6ec1e809bb66ec
Michał Gacek <gacek.michael@outlook.com>
2018-06-12 19:32:58 +0200

    Fixed.

--- webpack.config.js
+++ webpack.config.js
@@ -20,29 +20,11 @@ if (env === 'production') {
     );
 }
 
-const plugins = [
-    new HtmlWebpackPlugin({
-        template: 'client/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    }),
-    new UglifyJSPlugin()
-]
-
 module.exports = {
-<<<<<<< HEAD
-    devtool: 'source-map',
-    entry: (env !== 'production' ? [
-            'react-hot-loader/patch',
-            'webpack-dev-server/client?http://localhost:8080',
-            'webpack/hot/only-dev-server',
-        ] : []).concat(['./client/index.js']),
-=======
     entry: [
         'react-hot-loader/patch',
         './client/index.js'
     ],
->>>>>>> d83955830540d270cf604b07eca06d4d165fa7b6
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
@@ -68,9 +50,6 @@ module.exports = {
             }
         ]
     },
-<<<<<<< HEAD
-    plugins: plugins
-=======
     devServer: {
         proxy: {
             '/socket.io': {
@@ -80,5 +59,4 @@ module.exports = {
         }
     },
     plugins
->>>>>>> d83955830540d270cf604b07eca06d4d165fa7b6
 };
